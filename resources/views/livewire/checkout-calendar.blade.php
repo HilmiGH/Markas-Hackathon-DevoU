77ad098d57aac3cdb442a5f8a133f91b4f80d07a
Hilmi G <hilmigh03@gmail.com>
2023-11-23 11:51:26 +0700

    V0.1.1

--- resources/views/livewire/checkout-calendar.blade.php
+++ resources/views/livewire/checkout-calendar.blade.php
@@ -1,11 +1,11 @@
 <div class="">
     <image src="{{ asset('images/bg-paper.png') }}" class="w-full -z-10 absolute" />
-    <section class="" style="padding-left: 5rem; padding-top: 3rem">
-        <button style="border-radius: 24px; padding-block: 26px; padding-inline: 55px; background-color: white; border: 2px solid #000;">
+    <section class="" style="padding-left: 5rem; padding-right: 5rem; padding-top: 3rem">
+        <button style="border-radius: 24px; padding-block: 16px; padding-inline: 45px; background-color: white; border: 2px solid #000;">
             <x-uiw-left class="h-6 w-6 inline-block mr-4"/>
             <span class="font-lexendMega">Kembali ke pemilihan ruangan</span>
         </button>
-        <div style="display: flex; margin-top: 3rem; margin-bottom: 4rem ; justify-content: center">
+        <div style="display: flex; margin-top: 3rem; margin-bottom: 3rem ; justify-content: center">
             <div class="font-lexendMega">
                 <span class="text-redMarkas">1 /</span>
                 <span class="">Pilih Tanggal</span> 
@@ -22,119 +22,119 @@
             </div>
         </div>
     </section>
-    <section style="padding-left: 5rem; margin-top: 3rem; margin-bottom: 1rem">
-        <div class="mb-4 flex justify-center">
+    <section style="padding-left: 5rem; margin-top: 3rem; margin-bottom: 1rem; padding-right: 5rem;">
+        <div class="mb-12 flex justify-center">
             <button>
                 <x-uiw-left class="h-6 w-6 inline-block mr-4"/>
             </button>
-            <h1 class="font-lexendMega font-bold">November 2023</h1>
+            <h1 class="font-lexendMega font-bold text-[24px]">November 2023</h1>
             <button>
                 <x-uiw-right class="h-6 w-6 inline-block ml-4"/>
             </button>
         </div>
-        <table class="font-lexendMega w-full" style="">
-            <tr class="bg-redMarkas">
-                <th class="" style="color: rgb(255 255 255); border-top-left-radius: 24px; padding-block: 26px; padding-inline: 47px">Minggu</th>
-                <th class="" style="color: rgb(255 255 255); padding-block: 26px; padding-inline: 47px">Senin</th>
-                <th class="" style="color: rgb(255 255 255); padding-block: 26px; padding-inline: 47px">Selasa</th>
-                <th class="" style="color: rgb(255 255 255); padding-block: 26px; padding-inline: 47px">Rabu</th>
-                <th class="" style="color: rgb(255 255 255); padding-block: 26px; padding-inline: 47px">Kamis</th>
-                <th class="" style="color: rgb(255 255 255); padding-block: 26px; padding-inline: 47px">Jumat</th>
-                <th class="" style="color: rgb(255 255 255); border-top-right-radius: 24px; padding-block: 26px; padding-inline: 47px">Sabtu</th>
+        <table class="font-lexendMega w-full border-collapse">
+            <tr class="bg-redMarkas" style="border-top-left-radius: 24px; border-top-right-radius: 24px;">
+                <th class="border-2 p-2 border-black" style="color: rgb(255 255 255); border-top-left-radius: 24px; padding-block: 26px; padding-inline: 47px">Minggu</th>
+                <th class="border-2 p-2 border-black" style="color: rgb(255 255 255); padding-block: 26px; padding-inline: 47px">Senin</th>
+                <th class="border-2 p-2 border-black" style="color: rgb(255 255 255); padding-block: 26px; padding-inline: 47px">Selasa</th>
+                <th class="border-2 p-2 border-black" style="color: rgb(255 255 255); padding-block: 26px; padding-inline: 47px">Rabu</th>
+                <th class="border-2 p-2 border-black" style="color: rgb(255 255 255); padding-block: 26px; padding-inline: 47px">Kamis</th>
+                <th class="border-2 p-2 border-black" style="color: rgb(255 255 255); padding-block: 26px; padding-inline: 47px">Jumat</th>
+                <th class="border-2 p-2 border-black" style="color: rgb(255 255 255); border-top-right-radius: 24px; padding-block: 26px; padding-inline: 47px">Sabtu</th>
             </tr>
-            <tr>
-                <td sty>29</td>
-                <td>30</td>
-                <td>31</td>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>4</td>
+            <tr class="bg-white h-[8rem]" style="border: 2px solid #000">
+                <td class=" align-top" style="padding: 0.5rem; border: 2px solid #000">29</td>
+                <td class=" align-top" style="padding: 0.5rem; border: 2px solid #000">30</td>
+                <td class=" align-top" style="padding: 0.5rem; border: 2px solid #000">31</td>
+                <td class=" align-top" style="padding: 0.5rem; border: 2px solid #000">1</td>
+                <td class=" align-top" style="padding: 0.5rem; border: 2px solid #000">2</td>
+                <td class=" align-top" style="padding: 0.5rem; border: 2px solid #000">3</td>
+                <td class=" align-top" style="padding: 0.5rem; border: 2px solid #000">4</td>
             </tr>
-            <tr>
-                <td>5</td>
-                <td>6</td>
-                <td>7</td>
-                <td>8</td>
-                <td><img src="" alt="">9</td>
-                <td><img src="" alt="">10</td>
-                <td>11</td>
+            <tr class="bg-white h-[8rem] border-black border-2">
+                <td class="align-top p-2 border-black border-2">5</td>
+                <td class="align-top p-2 border-black border-2">6</td>
+                <td class="align-top p-2 border-black border-2">7</td>
+                <td class="align-top p-2 border-black border-2">8</td>
+                <td class="align-top p-2 border-black border-2"><img src="" alt="">9</td>
+                <td class="align-top p-2 border-black border-2"><img src="" alt="">10</td>
+                <td class="align-top p-2 border-black border-2">11</td>
             </tr>
-            <tr>
-                <td>12</td>
-                <td>13</td>
-                <td>14</td>
-                <td>15</td>
-                <td>16</td>
-                <td>17</td>
-                <td>18</td>
+            <tr class="bg-white h-[8rem] border-black">
+                <td class="align-top p-2 border-black border-2">12</td>
+                <td class="align-top p-2 border-black border-2">13</td>
+                <td class="align-top p-2 border-black border-2">14</td>
+                <td class="align-top p-2 border-black border-2">15</td>
+                <td class="align-top p-2 border-black border-2 ">16</td>
+                <td class="align-top p-2 border-black border-2">17</td>
+                <td class="align-top p-2 border-black border-2">18</td>
             </tr>
-            <tr>
-                <td>19</td>
-                <td>20</td>
-                <td><img src="" alt="">21</td>
-                <td><img src="" alt="">22</td>
-                <td><img src="" alt="">23</td>
-                <td>24</td>
-                <td>25</td>
+            <tr class="bg-white h-[8rem] border-black">
+                <td class="align-top p-2 border-black border-2">19</td>
+                <td class="align-top p-2 border-black border-2">20</td>
+                <td class="align-top p-2 border-black border-2"><img src="" alt="">21</td>
+                <td class="align-top p-2 border-black border-2"><img src="" alt="">22</td>
+                <td class="align-top p-2 border-black border-2"><img src="" alt="">23</td>
+                <td class="align-top p-2 border-black border-2">24</td>
+                <td class="align-top p-2 border-black border-2">25</td>
             </tr>
-            <tr>
-                <td style="border-bottom-left-radius: 24px">26</td>
-                <td>27</td>
-                <td>28</td>
-                <td>29</td>
-                <td>30</td>
-                <td>1</td>
-                <td style="border-bottom-right-radius: 24px">2</td>
+            <tr class="bg-white h-[8rem] border-black border-2" style="border-bottom-left-radius: 24px; border-bottom-right-radius: 24px; ">
+                <td class="align-top p-2 border-black border-2" style="border-bottom-left-radius: 24px">26</td>
+                <td class="align-top p-2 border-black border-2">27</td>
+                <td class="align-top p-2 border-black border-2">28</td>
+                <td class="align-top p-2 border-black border-2">29</td>
+                <td class="align-top p-2 border-black border-2">30</td>
+                <td class="align-top p-2 border-black border-2">1</td>
+                <td class="align-top p-2 border-black border-2" style="border-bottom-right-radius: 24px">2</td>
             </tr>
         </table>
     </section>
-    <section style="padding-left: 5rem; margin-top: 5rem">
-        <table class="font-lexendMega">
-            <tr>
-                <th class="bg-redMarkas" style="color: rgb(255 255 255); border-top-left-radius: 24px; border-top-right-radius: 24px">
-                    <span>21 November 2023</span>
-                    <x-fluentui-arrow-clockwise-20-o class="h-6 w-6 inline-block" />
+    <section style="padding-left: 5rem; padding-right: 5rem; margin-top: 5rem">
+        <table class="font-lexendMega w-full border-collapse">
+            <tr class="border-black">
+                <th class="bg-redMarkas p-4 w-max border-black" style="color: rgb(255 255 255); border-top-left-radius: 24px; border-top-right-radius: 24px;">
+                    <span class="float-left">21 November 2023</span>
+                    <x-fluentui-arrow-clockwise-20-o class="h-6 w-6 inline-block float-right" />
                 </th>
             </tr>
-            <tr class="rounded-b-[24px]">
-                <table>
-                    <tr>
-                        <th>Jam</th>
-                        <th>Kegiatan</th>
+            <tr class="rounded-b-[24px] border-black">
+                <table class="w-full border-collapse" style="padding: 5rem">
+                    <tr class="border-black">
+                        <th class="font-bold border-black">Jam</th>
+                        <th class="font-bold border-black">Kegiatan</th>
                     </tr>
-                    <tr>
-                        <td>12:00 - 14.00</td>
-                        <td></td>
+                    <tr class="border-black">
+                        <td class="border-black">12:00 - 14.00</td>
+                        <td class="border-black"></td>
                     </tr>
-                    <tr>
-                        <td>14:00 - 16.00</td>
-                        <td></td>
+                    <tr class="border-black">
+                        <td class="border-black">14:00 - 16.00</td>
+                        <td class="border-black"></td>
                     </tr>
-                    <tr>
-                        <td>16:00 - 18:00</td>
-                        <td><img src="" alt="">Sekolah Beta Intensif Hipster</td>
+                    <tr class="border-black">
+                        <td class="border-black">16:00 - 18:00</td>
+                        <td class="border-black"><img src="" alt="">Sekolah Beta Intensif Hipster</td>
                     </tr>
-                    <tr>
-                        <td>18:00 - 20:00</td>
-                        <td><img src="" alt="">Hackathon Build Web Markas</td>
+                    <tr class="border-black">
+                        <td class="border-black">18:00 - 20:00</td>
+                        <td class="border-black"><img src="" alt="">Hackathon Build Web Markas</td>
                     </tr>
-                    <tr>
-                        <td>20:00 - 22:00</td>
-                        <td><img src="" alt=""></td>
+                    <tr class="border-black">
+                        <td class="border-black">20:00 - 22:00</td>
+                        <td class="border-black"><img src="" alt=""></td>
                     </tr>
-                    <tr>
-                        <td>22:30 - 23:00</td>
-                        <td><img src="" alt=""></td>
+                    <tr class="border-black">
+                        <td class="border-black">22:30 - 23:00</td>
+                        <td class="border-black"><img src="" alt=""></td>
                     </tr>
-                    <tr>
-                        <td>23:00 - 23:30</td>
-                        <td><img src="" alt=""></td>
+                    <tr class="border-black">
+                        <td class="border-black">23:00 - 23:30</td>
+                        <td class="border-black"><img src="" alt=""></td>
                     </tr>
                 </table>
             </tr>
         </table>
-        <button class="bg-redMarkas">
+        <button class="bg-redMarkas mt-8">
             <span class="font-lexendMega" style="color: rgb(255 255 255);">
                 Pilih Tanggal
             </span>
